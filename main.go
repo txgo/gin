2620994a5431f4b85cd59ace9334108b6805e011
txgo <tianxi.liu@gmail.com>
2023-04-04 10:14:29 +0800

    Forward prompt to open and sent back completion

--- main.go
+++ main.go
@@ -2,84 +2,148 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	sdkginext "github.com/larksuite/oapi-sdk-gin"
+	lark "github.com/larksuite/oapi-sdk-go/v3"
 	larkcard "github.com/larksuite/oapi-sdk-go/v3/card"
 	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	"github.com/larksuite/oapi-sdk-go/v3/event/dispatcher"
 	larkcontact "github.com/larksuite/oapi-sdk-go/v3/service/contact/v3"
 	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 type Config struct {
-	APP_ID                 string
-	APP_SECRET             string
-	APP_ENCRYPT_KEY        string
-	APP_VERIFICATION_TOKEN string
-	BOT_NAME               string
-	GIN_MODE               string
-	HTTP_PORT              string
-	PORT                   string
-	OPENAI_KEY             string
+	AppID             string
+	AppSecret         string
+	EncryptKey        string
+	VerificationToken string
+	BotName           string
+	GinMode           string
+	Port              string
+	HttpPort          string
+	OpenAIKey         string
 }
 
+var client *lark.Client
+
 func main() {
-	// create a new config struct
-	config := Config{}
+	// read the configuration options from environment variables
+	config := &Config{
+		AppID:             os.Getenv("APP_ID"),
+		AppSecret:         os.Getenv("APP_SECRET"),
+		EncryptKey:        os.Getenv("ENCRYPT_KEY"),
+		VerificationToken: os.Getenv("VERIFICATION_TOKEN"),
+		BotName:           os.Getenv("BOT_NAME"),
+		GinMode:           os.Getenv("GIN_MODE"),
+		Port:              os.Getenv("PORT"),
+		HttpPort:          os.Getenv("HTTP_PORT"),
+		OpenAIKey:         os.Getenv("OPENAI_API_KEY"),
+	}
 
-	// get environment variables and set them in config struct
-	config.APP_ID = os.Getenv("APP_ID")
-	config.APP_SECRET = os.Getenv("APP_SECRET")
-	config.APP_ENCRYPT_KEY = os.Getenv("APP_ENCRYPT_KEY")
-	config.APP_VERIFICATION_TOKEN = os.Getenv("APP_VERIFICATION_TOKEN")
-	config.BOT_NAME = os.Getenv("BOT_NAME")
-	config.GIN_MODE = os.Getenv("GIN_MODE")
-	config.PORT = os.Getenv("PORT")
-	config.HTTP_PORT = os.Getenv("HTTP_PORT")
-	config.OPENAI_KEY = os.Getenv("OPENAI_KEY")
+	// initialize the OpenAI API client
+	openaiClient := openai.NewClient(config.OpenAIKey)
 
-	fmt.Printf("+%v %v %v\n", config.BOT_NAME, config.PORT, config.HTTP_PORT)
+	// create a new Lark IM client
+	client = lark.NewClient(config.AppID, config.AppSecret)
 
-	// 创建注册消息处理器
-	handler := dispatcher.NewEventDispatcher(config.APP_VERIFICATION_TOKEN, config.APP_ENCRYPT_KEY).OnP2MessageReceiveV1(func(ctx context.Context, event *larkim.P2MessageReceiveV1) error {
-		fmt.Println(larkcore.Prettify(event))
-		fmt.Println(event.RequestId())
-		return nil
-	}).OnP2MessageReadV1(func(ctx context.Context, event *larkim.P2MessageReadV1) error {
-		fmt.Println(larkcore.Prettify(event))
-		fmt.Println(event.RequestId())
-		return nil
-	}).OnP2UserCreatedV3(func(ctx context.Context, event *larkcontact.P2UserCreatedV3) error {
-		fmt.Println(larkcore.Prettify(event))
-		fmt.Println(event.RequestId())
-		return nil
-	})
+	// create an event dispatcher
+	eventDispatcher := dispatcher.NewEventDispatcher(config.VerificationToken, config.EncryptKey).
+		OnP2MessageReceiveV1(func(ctx context.Context, event *larkim.P2MessageReceiveV1) error {
+			log.Printf("Received P2 message receive event: %v", larkcore.Prettify(event))
 
-	// 创建卡片行为处理器
-	cardHandler := larkcard.NewCardActionHandler(config.APP_VERIFICATION_TOKEN, config.APP_ENCRYPT_KEY, func(ctx context.Context, cardAction *larkcard.CardAction) (interface{}, error) {
-		fmt.Println(larkcore.Prettify(cardAction))
+			// generate a response using the OpenAI API
+			completion, err := generateResponse(ctx, *event.Event.Message.Content, openaiClient)
+			if err != nil {
+				log.Printf("Error generating response: %v", err)
+				return nil
+			}
 
-		// 返回卡片消息
-		//return getCard(), nil
+			log.Printf("Received Openai completion: %v", larkcore.Prettify(completion))
 
-		//custom resp
-		//return getCustomResp(),nil
+			respMsg := larkim.NewTextMsgBuilder().Text(completion).Build()
 
-		// 无返回值
+			chatId := event.Event.Message.ChatId
+			resp, err := client.Im.Message.Create(context.Background(), larkim.NewCreateMessageReqBuilder().
+				ReceiveIdType(larkim.ReceiveIdTypeChatId).
+				Body(larkim.NewCreateMessageReqBodyBuilder().
+					MsgType(larkim.MsgTypeText).
+					ReceiveId(*chatId).
+					Content(respMsg).
+					Build()).
+				Build())
+
+			if err != nil {
+				log.Printf("Error Send Completion to Lark: %v", err)
+				return nil
+			}
+			if !resp.Success() {
+				log.Printf("Error sending message: %v", resp.Code)
+				log.Printf("Error message: %s", resp.Msg)
+				return nil
+			}
+
+			return nil
+		}).
+		OnP2MessageReadV1(func(ctx context.Context, event *larkim.P2MessageReadV1) error {
+			log.Printf("Received P2 message read event: %v", larkcore.Prettify(event))
+			return nil
+		}).
+		OnP2UserCreatedV3(func(ctx context.Context, event *larkcontact.P2UserCreatedV3) error {
+			log.Printf("Received P2 user created event: %v", larkcore.Prettify(event))
+			return nil
+		})
+
+	// create a card action handler
+	cardActionHandler := larkcard.NewCardActionHandler(config.VerificationToken, config.EncryptKey, func(ctx context.Context, cardAction *larkcard.CardAction) (interface{}, error) {
+		log.Printf("Received card action: %v", larkcore.Prettify(cardAction))
+
+		// TODO: handle the card action and return a response
 		return nil, nil
 	})
 
-	// 注册处理器
-	g := gin.Default()
-	g.POST("/webhook/event", sdkginext.NewEventHandlerFunc(handler))
-	g.POST("/webhook/card", sdkginext.NewCardActionHandlerFunc(cardHandler))
+	// set up the Gin router
+	router := gin.Default()
 
-	// 启动服务
-	err := g.Run(":" + config.PORT)
+	// register the event and card action handlers
+	router.POST("/webhook/event", sdkginext.NewEventHandlerFunc(eventDispatcher))
+	router.POST("/webhook/card", sdkginext.NewCardActionHandlerFunc(cardActionHandler))
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	err := router.Run(":" + config.Port)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error starting server: %v", err)
 	}
+
+}
+
+// generateResponse uses the OpenAI API to generate a response to the user's message.
+func generateResponse(ctx context.Context, message string, client *openai.Client) (string, error) {
+	// set up the prompt and parameters
+	completionRequest := &openai.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo0301,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: message,
+			},
+		},
+	}
+
+	// generate the completion using the OpenAI API
+	response, err := client.CreateChatCompletion(ctx, *completionRequest)
+	if err != nil {
+		return "", err
+	}
+
+	// return the generated text
+	return response.Choices[0].Message.Content, nil
 }
